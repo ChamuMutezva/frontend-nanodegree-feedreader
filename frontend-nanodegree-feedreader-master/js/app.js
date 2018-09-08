e2103869ad22f553b2a4cad32a1efaa317952144
Chamunorwa Mutezva <ckmutezva@gmail.com>
2018-09-08 04:59:02 +0200

    Test that loops through each feed
    a test that loops through each feed in the allFeeds object and ensures it has a URL defined and that the URL is not empty. Fixed.

--- frontend-nanodegree-feedreader-master/js/app.js
+++ frontend-nanodegree-feedreader-master/js/app.js
@@ -8,8 +8,8 @@
 
 // The names and URLs to all of the feeds we'd like available.
 var allFeeds = [
-    {
-        name: 'Udacity Blog',
+   {
+       name: 'Udacity Blog',
         url: 'http://blog.udacity.com/feed'
     }, {
         name: 'CSS Tricks',
